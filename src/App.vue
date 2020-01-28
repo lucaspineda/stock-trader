0f0a3586d2223daebbfdf0a8a45e68a91c20d265
Lucas <lucas.pineda@hotmail.com>
2020-01-28 07:16:55 -0300

    create transition between components

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
     <div id="app">
       <Header />
       <div class="box-screen">
-        <router-view></router-view>
+        <transition name="slide" mode="out-in">
+          <router-view></router-view>
+        </transition>
       </div>
     </div>
   </v-app>
@@ -24,3 +26,19 @@ export default {
   }),
 };
 </script>
+<style>
+  @keyframes slide-in {
+    from { transform: translateY(-30px); opacity: 0; }
+    to { transform: translateY(0); opacity: 1; }
+  }
+  @keyframes slide-out {
+    from { transform: translateY(0); opacity: 1; }
+    to { transform: translateY(-30); opacity: 0; }
+  }
+  .slide-enter-active {
+    animation: slide-in 0.3s ease;
+  }
+  .slide-leave-active {
+    animation: slide-out 0.3s ease;
+  }
+</style>
